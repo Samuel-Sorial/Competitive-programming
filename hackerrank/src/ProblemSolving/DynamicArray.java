98bd1cd878f9c648d1379b3875ed0ab3da7c4e3c
Samuel-Sorial <eng.samuel.maged@gmail.com>
2020-04-06 02:34:27 +0200

    Adding documentation to Dynamic Array problem

--- hackerrank/src/ProblemSolving/DynamicArray.java
+++ hackerrank/src/ProblemSolving/DynamicArray.java
@@ -11,22 +11,22 @@ public class DynamicArray {
      * @author : Samuel-Sorial
      */
     public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
-        List<List<Integer>> sequences = new ArrayList<>(n);
+        List<List<Integer>> sequences = new ArrayList<>(); //Create 2 sequences to store the values
         List<Integer> first = new ArrayList<>();
         List<Integer> second = new ArrayList<>();
         List<Integer> lastAnswers = new ArrayList<>();
         sequences.add(first);
         sequences.add(second);
-        int lastAnswer = 0;
+        int lastAnswer = 0; // Initialize the last answer
         for(int i = 0; i<n; i++){
-            List<Integer> operands = queries.get(i);
+            List<Integer> operands = queries.get(i); //Get the list of this line
             int typeOfQuery = operands.get(0);
             int x = operands.get(1);
             int y = operands.get(2);
-            int currIndexOfSequences = (x^lastAnswer) % n;
+            int currIndexOfSequences = (x^lastAnswer) % n; // Find the index of the sequence
             if(typeOfQuery == 1){
-                sequences.get(currIndexOfSequences).add(y);
-            }else{
+                sequences.get(currIndexOfSequences).add(y); // Execute query 1
+            }else{  // Exectue query 2
                 int sequence = (x^lastAnswer) %2;
                 int element = y % sequences.get(sequence).size();
                 lastAnswer =sequences.get(sequence).get(element) ;
