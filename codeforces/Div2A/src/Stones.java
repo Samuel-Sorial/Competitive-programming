3163dad7f1b9912071f2c82981c45cd8dfe90195
Samuel-Sorial <eng.samuel.maged@gmail.com>
2020-05-04 17:55:38 +0200

    Stones on the table accepted.

---
+++ codeforces/Div2A/src/Stones.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stones {
+    /* https://codeforces.com/contest/266/problem/A */
+    public static void main(String args[]){
+        Scanner in  = new Scanner(System.in);
+        int totalChars = in.nextInt();
+        char[] chars = in.next().toCharArray();
+        System.out.print(pullSame(chars));
+    }
+    private static int pullSame(char[] chars ) {
+        int pulledOut = 0;
+        char prev = 'd';
+        for(int i = 0; i < chars.length; i++){
+            if(i == 0) {
+                prev = chars[i];
+                continue;
+            }
+            if(prev == chars[i]) {
+                pulledOut++;
+                continue;
+            }
+            prev = chars[i];
+        }
+        return pulledOut;
+    }
+
+}
