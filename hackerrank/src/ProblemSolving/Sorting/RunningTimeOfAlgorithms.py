c5265a931c4f3ab14fb93fa76e2fa42acd902358
Samuel-Sorial <eng.samuel.maged@gmail.com>
2020-11-27 03:54:54 +0200

    Solving running time problem

---
+++ hackerrank/src/ProblemSolving/Sorting/RunningTimeOfAlgorithms.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# https://www.hackerrank.com/challenges/runningtime/problem
+
+def runningTime(arr):
+    shifts = 0
+    for i in range(1, len(arr)):
+        j = i-1
+        key = arr[i]
+        while (j >= 0) and (arr[j] > key):
+           arr[j+1] = arr[j]
+           j -= 1
+           shifts += 1
+        arr[j+1] = key
+    return shifts
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
