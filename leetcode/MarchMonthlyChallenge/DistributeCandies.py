3eca129fbc8cfb507642cd889e0ac24d23317181
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-01 17:23:00 +0200

    Adding syntactic sugar to the solution

--- leetcode/MarchMonthlyChallenge/DistributeCandies.py
+++ leetcode/MarchMonthlyChallenge/DistributeCandies.py
@@ -3,4 +3,4 @@ class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         doctorMaximum = len(candyType) // 2
         differentTypes = len(set(candyType))
-        return differentTypes if differentTypes < doctorMaximum else doctorMaximum
+        return min(doctorMaximum, differentTypes)
